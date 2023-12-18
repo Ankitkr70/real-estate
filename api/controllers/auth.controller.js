b6fec832e7405f4189f25521973d7163212b086e
AnkitKumar <ankitkr70@gmail.com>
2023-12-18 19:41:47 +0530

    sign in api with jwt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../../models/user.model.js";
 import { customError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 //Sign UP function
 export const signup = async (req, res, next) => {
@@ -21,3 +22,21 @@ export const signup = async (req, res, next) => {
 };
 
 //Sign In function
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(customError(404, "User doen't exists!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(customError(201, "Wrong Credentials!"));
+    const accessToken = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: userPassword, ...data } = validUser._doc;
+    res
+      .cookie("access_token", accessToken, { httpOnly: true })
+      .status(200)
+      .json(data);
+  } catch (error) {
+    next(error);
+  }
+};
